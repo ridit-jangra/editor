a3628673e70c3cf1b345e0d5959788b597b62bb7
Ridit Jangra <riditjangra09@gmail.com>
2026-04-20 18:04:17 +0530

    chore: add exports

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,5 @@
+export { h } from "./utils/h";
+export { cn } from "./utils/cn";
+export { Button } from "./components/Button/index";
+export { Tooltip } from "./components/Tooltip/index";
+export { Splitter } from "./components/Splitter/splitter";
