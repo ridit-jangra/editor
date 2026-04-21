df2e2cd68069047121e87cfcffc13137d54d9b30
Ridit Jangra <riditjangra09@gmail.com>
2026-04-21 19:41:30 +0530

    feat: add channels

---
+++ packages/services/src/emitter/channels.ts
@@ -0,0 +1,6 @@
+export const STATUSBAR_SET_MESSAGE = "statusbar:setMessage";
+export const STATUSBAR_SET_LINE_COL = "statusbar:setLineCol";
+export const STATUSBAR_SET_LANGUAGE = "statusbar:setLanguage";
+export const STATUSBAR_SET_INDENTATION = "statusbar:setIndentation";
+export const STATUSBAR_SET_ENCODING = "statusbar:setEncoding";
+export const STATUSBAR_SET_FILENAME = "statusbar:setFileName";
