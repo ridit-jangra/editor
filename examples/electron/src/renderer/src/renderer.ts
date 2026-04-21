066c5d18cc53fca7126eb058b139a127e0224c71
Ridit Jangra <riditjangra09@gmail.com>
2026-04-21 17:48:02 +0530

    chore: update electron example

--- examples/electron/src/renderer/src/renderer.ts
+++ examples/electron/src/renderer/src/renderer.ts
@@ -3,12 +3,17 @@ import {
   EventEmitter,
   FileSystemService,
   LspService,
-  WorkbenchService
+  WorkbenchService,
+  ExplorerService,
+  StorageService
 } from '@ridit/editor-services/browser'
 
 function init(): void {
   window.addEventListener('DOMContentLoaded', async () => {
     const eventEmitter = new EventEmitter()
+
+    const storageService = new StorageService()
+    storageService.start(window, 'electron')
 
     const lspService = new LspService(eventEmitter, {
       disableInBuiltTypescriptWorker: true,
@@ -20,20 +25,44 @@ function init(): void {
       name: 'MyVirtualSystem'
     })
 
-    fileSystem.writeFile(
+    await fileSystem.writeFile(
       'E:\\projects\\editor\\examples\\electron\\src\\renderer\\src\\renderer.py',
-      "print('Hello World!!!!')"
+      "print('Hello Worldd asd asd asd!!!!')"
     )
+
+    await fileSystem.writeFile(
+      'E:\\projects\\editor\\examples\\electron\\src\\renderer\\src\\renderer.css',
+      "print('Hello Worl asda sdasd asd!!!!')"
+    )
+
+    await fileSystem.writeFile(
+      'E:\\projects\\editor\\examples\\electron\\src\\renderer\\src\\renderer.html',
+      "print('Hello World! asdasdas da!!!')"
+    )
+
+    const explorerService = new ExplorerService(eventEmitter, {
+      fileSystem: fileSystem,
+      rootPath: 'E:\\projects\\editor\\examples\\electron',
+      async onFileOpen(path) {
+        const model = await editorService.create_model(path)
+        await editorService.set_model_active(model.uri)
+      }
+    })
 
     const editorService = new EditorService(eventEmitter, {
       LspService: lspService,
       fileSystem,
+      explorerService,
+      storageService,
       editorConfig: {
         fontSize: 24
       }
     })
+
     const workbenchService = new WorkbenchService(eventEmitter, {
       editorService,
+      explorerService,
+      storageService,
       config: {
         fontSize: {
           size: 18,
@@ -43,7 +72,7 @@ function init(): void {
       }
     })
 
-    workbenchService.mount(document, window)
+    await workbenchService.mount(document, window)
 
     const model = await editorService.create_model(
       'E:\\projects\\editor\\examples\\electron\\src\\renderer\\src\\renderer.py'
