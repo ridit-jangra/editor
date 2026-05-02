c9d5f891363f843c97a7fb3b535fa3aa32dc81eb
Ridit Jangra <riditjangra09@gmail.com>
2026-05-02 12:42:39 +0530

    feat: add tokenColors

--- packages/services/src/ThemeService/index.ts
+++ packages/services/src/ThemeService/index.ts
@@ -4,32 +4,72 @@ import { DarkTheme } from "../WorkbenchService/basic-presets/themes";
 import { themeToVariables } from "./utils";
 
 export interface ITheme {
-  bg: string;
-  fg: string;
-  editorBg: string;
-  editorFg: string;
-  tabBg: string;
-  tabFg: string;
-  tabActiveBg: string;
-  tabActiveFg: string;
-  tabHoverBg: string;
-  tabHoverFg: string;
-  activityBg: string;
-  activityFg: string;
-  activityHoverBg: string;
-  activityHoverFg: string;
-  actvityActiveBg: string;
-  actvityActiveFg: string;
-  sidebarBg: string;
-  sidebarFg: string;
-  statusBg: string;
-  statusFg: string;
-  statusHoverBg: string;
-  statusHoverFg: string;
-  splitHandle: string;
-  splitHandleHover: string;
-  splitHandleActive: string;
+  kind: "dark" | "light";
+  colors: {
+    bg: string;
+    fg: string;
+    border: string;
+    editorBg: string;
+    editorFg: string;
+    tabBg: string;
+    tabFg: string;
+    tabActiveBg: string;
+    tabActiveBorder: string;
+    tabActiveFg: string;
+    tabHoverBg: string;
+    tabHoverFg: string;
+    activityBg: string;
+    activityFg: string;
+    activityHoverBg: string;
+    activityHoverFg: string;
+    actvityActiveBg: string;
+    actvityActiveFg: string;
+    sidebarBg: string;
+    sidebarFg: string;
+    statusBg: string;
+    statusFg: string;
+    statusHoverBg: string;
+    statusHoverFg: string;
+    splitHandle: string;
+    splitHandleHover: string;
+    splitHandleActive: string;
+  };
+  tokens: {
+    default: string;
+    keyword: string;
+    "keyword.json": string;
+    source: string;
+    "keyword.typeModifier": string;
+    metadata: string;
+    number: string;
+    boolean: string;
+    string: string;
+    "string.binary": string;
+    "string.escape": string;
+    "string.escape.alternative": string;
+    "string.format.item": string;
+    "string.regexp": string;
+    identifier: string;
+    "identifier.this": string;
+    "identifier.constant": string;
+    "identifier.variable.local": string;
+    "identifier.parameter": string;
+    "identifier.function.declaration": string;
+    "identifier.method.static": string;
+    "identifier.builtin": string;
+    "identifier.type": string;
+    "identifier.field": string;
+    "identifier.field.static": string;
+    "identifier.interface": string;
+    "identifier.type.class": string;
+    comment: string;
+    "comment.parameter": string;
+    punctuation: string;
+  };
 }
+
+export type ThemeColors = ITheme["colors"];
+export type ThemeTokens = ITheme["tokens"];
 
 export class ThemeService extends Service {
   private theme: ITheme;
@@ -46,6 +86,14 @@ export class ThemeService extends Service {
   setTheme(theme: ITheme, document: Document) {
     this.theme = theme;
     this.injectVariables(document);
+  }
+
+  getToken<K extends keyof ThemeTokens>(token: K): ThemeTokens[K] {
+    return this.theme.tokens[token];
+  }
+
+  getColor<K extends keyof ThemeColors>(token: K): ThemeColors[K] {
+    return this.theme.colors[token];
   }
 
   private injectVariables(document: Document) {
--- packages/services/src/ThemeService/utils.ts
+++ packages/services/src/ThemeService/utils.ts
@@ -1,33 +1,36 @@
 import { ITheme } from ".";
 
 export function themeToVariables(theme: ITheme): string {
+  const colors = theme.colors;
   return `
       :root {
-        --wb-bg: ${theme.bg};
-        --wb-fg: ${theme.fg};
-        --wb-editor-bg: ${theme.editorBg};
-        --wb-editor-fg: ${theme.editorFg};
-        --wb-tab-bg: ${theme.tabBg};
-        --wb-tab-fg: ${theme.tabFg};
-        --wb-tab-active-bg: ${theme.tabActiveBg};
-        --wb-tab-active-fg: ${theme.tabActiveFg};
-        --wb-tab-hover-bg: ${theme.tabHoverBg};
-        --wb-tab-hover-fg: ${theme.tabHoverFg};
-        --wb-activity-bg: ${theme.activityBg};
-        --wb-activity-fg: ${theme.activityFg};
-        --wb-activity-hover-bg: ${theme.activityHoverBg};
-        --wb-activity-hover-fg: ${theme.activityHoverFg};
-        --wb-activity-active-bg: ${theme.actvityActiveBg};
-        --wb-activity-active-fg: ${theme.actvityActiveFg};
-        --wb-status-bg: ${theme.statusBg};
-        --wb-status-fg: ${theme.statusFg};
-        --wb-sidebar-bg: ${theme.sidebarBg};
-        --wb-sidebar-fg: ${theme.sidebarFg};
-        --wb-status-hover-bg: ${theme.statusHoverBg};
-        --wb-status-hover-fg: ${theme.statusHoverFg};
-        --wb-split-handle: ${theme.splitHandle};
-        --wb-split-handle-hover: ${theme.splitHandleHover};
-        --wb-split-handle-active: ${theme.splitHandleActive};
+        --wb-bg: ${colors.bg};
+        --wb-fg: ${colors.fg};
+        --wb-border: ${colors.border};
+        --wb-editor-bg: ${colors.editorBg};
+        --wb-editor-fg: ${colors.editorFg};
+        --wb-tab-bg: ${colors.tabBg};
+        --wb-tab-fg: ${colors.tabFg};
+        --wb-tab-active-bg: ${colors.tabActiveBg};
+        --wb-tab-active-border: ${colors.tabActiveBorder};
+        --wb-tab-active-fg: ${colors.tabActiveFg};
+        --wb-tab-hover-bg: ${colors.tabHoverBg};
+        --wb-tab-hover-fg: ${colors.tabHoverFg};
+        --wb-activity-bg: ${colors.activityBg};
+        --wb-activity-fg: ${colors.activityFg};
+        --wb-activity-hover-bg: ${colors.activityHoverBg};
+        --wb-activity-hover-fg: ${colors.activityHoverFg};
+        --wb-activity-active-bg: ${colors.actvityActiveBg};
+        --wb-activity-active-fg: ${colors.actvityActiveFg};
+        --wb-status-bg: ${colors.statusBg};
+        --wb-status-fg: ${colors.statusFg};
+        --wb-sidebar-bg: ${colors.sidebarBg};
+        --wb-sidebar-fg: ${colors.sidebarFg};
+        --wb-status-hover-bg: ${colors.statusHoverBg};
+        --wb-status-hover-fg: ${colors.statusHoverFg};
+        --wb-split-handle: ${colors.splitHandle};
+        --wb-split-handle-hover: ${colors.splitHandleHover};
+        --wb-split-handle-active: ${colors.splitHandleActive};
       }
     `;
 }
