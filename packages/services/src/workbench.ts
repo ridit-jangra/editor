a2d679e7c74db96bb2b1992e5914c31b788af862
Ridit Jangra <riditjangra09@gmail.com>
2026-05-02 15:47:19 +0530

    feat: add Workbench preset factory with createElectron and createWeb

---
+++ packages/services/src/workbench.ts
@@ -0,0 +1,131 @@
+import { EditorService } from "./EditorService";
+import { MonacoEditorOptions } from "./EditorService/default-editors/monaco";
+import { EventEmitter } from "./emitter";
+import { ExplorerService } from "./ExplorerService";
+import { FileSystemService } from "./FileSystemService";
+import { LspService } from "./LspService";
+import { StorageService } from "./StorageService";
+import { ThemeService } from "./ThemeService";
+import { BasicTheme, WorkbenchConfig } from "./types";
+import { WorkbenchService } from "./WorkbenchService";
+
+export type WebPresetOptions = {
+  rootPath?: string;
+  config?: Partial<WorkbenchConfig>;
+  editorConfig?: MonacoEditorOptions;
+  theme?: BasicTheme;
+  storeName?: string;
+  virtualFsName?: string;
+};
+
+export type ElectronPresetOptions = {
+  rootPath: string;
+  lsp?: {
+    disableInBuiltTypescriptWorker?: boolean;
+  };
+  config?: Partial<WorkbenchConfig>;
+  editorConfig?: MonacoEditorOptions;
+  theme?: BasicTheme;
+  storeName?: string;
+};
+
+export class Workbench {
+  static async createElectron(
+    opts: ElectronPresetOptions,
+  ): Promise<WorkbenchService> {
+    const eventEmitter = new EventEmitter();
+
+    const storageService = new StorageService(
+      window,
+      "electron",
+      opts.storeName ?? "workbench",
+    );
+    await storageService.start();
+
+    const lspService = opts.lsp
+      ? new LspService(eventEmitter, {
+          disableInBuiltTypescriptWorker:
+            opts.lsp.disableInBuiltTypescriptWorker ?? false,
+          defaultWorkspaceFolder: opts.rootPath,
+        })
+      : undefined;
+
+    const fileSystem = new FileSystemService(eventEmitter, window, {
+      mode: "real",
+    });
+
+    const explorerService = new ExplorerService(eventEmitter, {
+      services: { fileSystem },
+      rootPath: opts.rootPath,
+    });
+
+    const themeService = new ThemeService(eventEmitter);
+
+    const editorService = new EditorService(eventEmitter, {
+      services: {
+        lspService,
+        fileSystem,
+        explorerService,
+        themeService,
+        storageService,
+      },
+      theme: opts.theme ?? "Dark",
+      editorConfig: opts.editorConfig,
+    });
+
+    return new WorkbenchService(eventEmitter, {
+      services: {
+        editorService,
+        explorerService,
+        storageService,
+        themeService,
+      },
+      config: opts.config,
+      theme: opts.theme ?? "Dark",
+    });
+  }
+
+  static async createWeb(
+    opts: WebPresetOptions = {},
+  ): Promise<WorkbenchService> {
+    const eventEmitter = new EventEmitter();
+
+    const storageService = new StorageService(
+      window,
+      "web",
+      opts.storeName ?? "workbench",
+    );
+    await storageService.start();
+
+    const fileSystem = new FileSystemService(eventEmitter, window, {
+      mode: "virtual",
+      name: opts.virtualFsName ?? "default",
+    });
+
+    const rootPath = opts.rootPath ?? "/";
+
+    const explorerService = new ExplorerService(eventEmitter, {
+      services: { fileSystem },
+      rootPath,
+    });
+
+    const themeService = new ThemeService(eventEmitter);
+
+    const editorService = new EditorService(eventEmitter, {
+      services: { fileSystem, explorerService, themeService, storageService },
+      theme: opts.theme ?? "Dark",
+      editorConfig: opts.editorConfig,
+    });
+
+    return new WorkbenchService(eventEmitter, {
+      services: {
+        editorService,
+        explorerService,
+        storageService,
+        themeService,
+      },
+      config: opts.config,
+      theme: opts.theme ?? "Dark",
+    });
+  }
+}
